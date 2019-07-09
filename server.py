7690f3b284f40cbbc928cdb528e95514df6c5aaa
Sota-Watanabe <cy16179@shibaura-it.ac.jp>
2019-07-09 19:57:32 +0900

    first commit

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "route"
+
+@app.route('/client')
+def cliant():
+    return render_template('client.html')
+
+@app.route('/make-task')
+def make():
+    info = {
+        "target": "100000",
+        "taskID": "1"
+    }
+    return jsonify(info)
+
+@app.route('/complete-task', methods=['GET', 'POST'])
+def complete():
+    print(request.form['taskID'])
+    print(request.form['result'])
+    return 'ok from server'
+
+if __name__ == "__main__":
+    app.run(threaded=True, debug=True)
