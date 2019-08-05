b00cfeb4d7d9f5c8204de2331bc33488db822f6e
kumbikumbiSIC <cy16171@shibaura-it.ac.jp>
2019-08-05 21:40:39 +0900

    change server.py

--- server.py
+++ server.py
@@ -149,4 +149,5 @@ def complete():
 
 # flask 設定
 if __name__ == "__main__":
-    app.run(threaded=True, debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
