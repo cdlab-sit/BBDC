a8246016fef028867f3bf720261e9fcc9af4369c
kumbikumbiSIC <cy16171@shibaura-it.ac.jp>
2019-08-08 15:22:09 +0900

    fix

--- server.py
+++ server.py
@@ -43,7 +43,7 @@ def host():
     # 2週目以降のために初期化
     global filename
     check_table=0
-    table = '\'(moguchan),\'' 
+    table = '(\'moguchan\'),' 
     cur.execute("SELECT relname FROM pg_stat_user_tables;")
     tables=cur.fetchall()
     
@@ -55,6 +55,7 @@ def host():
             print(i)
             print("tabel: " + table)
             if(i==table):
+                print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
                 check_table=1
 
     if(check_table==0):
